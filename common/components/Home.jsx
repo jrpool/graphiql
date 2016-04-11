89139bed9d5fef68d97f3b9557f4608c9b4dcd41
jeffreywescott <jeffrey@learnersguild.org>
2016-04-11 11:43:26 -0700

    handle service changes correctly

--- common/components/Home.jsx
+++ common/components/Home.jsx
@@ -1,3 +1,4 @@
+/* global __CLIENT__ */
 import './graphiQLHacks' // this is terrible, but GraphiQL doesn't support SSR
 import React, {Component, PropTypes} from 'react'
 
@@ -5,12 +6,45 @@ import GraphiQL from 'graphiql'
 
 import 'graphiql/graphiql.css'
 
+
+class ServiceStorage {
+  constructor(service) {
+    this.service = service
+  }
+
+  serviceKey(key) {
+    return `${this.service}:${key}`
+  }
+
+  getItem(key) {
+    if (__CLIENT__) {
+      return window.localStorage.getItem(this.serviceKey(key))
+    }
+  }
+
+  setItem(key, value) {
+    if (__CLIENT__) {
+      if (value) {
+        return window.localStorage.setItem(this.serviceKey(key), value)
+      }
+    }
+  }
+}
+
+
 export default class Home extends Component {
   render() {
     const {service, services, fetcher, onSelectService} = this.props
+    const storage = new ServiceStorage(service)
 
+    // pass the 'key' property so the component will re-mount whenever
+    // the service changes
     return (
-      <GraphiQL fetcher={fetcher}>
+      <GraphiQL
+        key={service}
+        fetcher={fetcher}
+        storage={storage}
+        >
         <GraphiQL.Toolbar>
           <label className="service-label">Service:</label>
           <select onChange={onSelectService} value={service}>
