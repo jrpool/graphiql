c6dd0f9f3a883605323997cf1a5d7b91db9e2455
Sabra Pratt <pratt.sabra@gmail.com>
2016-06-07 07:30:43 -0700

    properly load piping for non-hot reload envs

--- server/index.js
+++ server/index.js
@@ -8,15 +8,22 @@ require('babel-polyfill')
 require('dotenv').config({path: ENV_PATH, silent: true})
 
 const config = require('config')
-const piping = require('piping')
 
 // These may also be defined by webpack on the client-side.
 global.__CLIENT__ = false
 global.__SERVER__ = true
 
-if (!config.server.hotReload || piping()) {
-  // because we use piping; this file will be evaluated twice;
-  // ensure we only process setup once.
+function startServer() {
   require('./configureCSSModules')()
   require('./server').start()
+}
+
+if (config.server.hotReload) {
+  if (require('piping')()) {
+    // because we use piping; this file will be evaluated twice;
+    // ensure we only process setup once.
+    startServer()
+  }
+} else {
+  startServer()
 }
